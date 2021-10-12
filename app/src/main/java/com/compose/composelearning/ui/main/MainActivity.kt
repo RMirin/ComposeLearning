72143aad8469ade27ebffe2424b5ad23b7d1f52c
Roman Mirin <r.mirin@zorra.com>
2021-10-12 16:45:11 +0300

    card style for recipes added

--- app/src/main/java/com/compose/composelearning/ui/main/MainActivity.kt
+++ app/src/main/java/com/compose/composelearning/ui/main/MainActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -49,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                 BarkHomeContent((uiState.value as State.Success<List<RecipeDto>>).data)
                 Log.e("TAG", "ShowItems:")
             }
-            is State.Error -> {  }
+            is State.Error -> { }
             is State.Loading -> { }
         }
         // Launch a coroutine when the component is first launched
@@ -64,7 +65,7 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun BarkHomeContent(list: List<RecipeDto>) {
     LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        contentPadding = PaddingValues(vertical = 8.dp),
         modifier = Modifier.fillMaxSize()
     ) {
         items(
@@ -78,17 +79,28 @@ fun BarkHomeContent(list: List<RecipeDto>) {
 @ExperimentalAnimationApi
 @Composable
 fun RecipeListItem(recipe: RecipeDto) {
-    Row {
-        var extended by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier.clickable { extended = !extended }
-        ) {
+    var extended by remember { mutableStateOf(false) }
+    Card(
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier
+            .padding(start = 16.dp, top = 0.dp, bottom = 8.dp, end = 16.dp)
+            .clickable { extended = !extended }
+    ) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, top = 8.dp, bottom = 8.dp, end = 16.dp)
+    ) {
+        Column() {
             Text(text = recipe.title)
-            Text(text = "VIEW DETAIL")
+            AnimatedVisibility(visible = !extended) {
+                Text(text = "VIEW DETAIL")
+            }
             AnimatedVisibility(visible = extended) {
                 Text(text = recipe.summary)
             }
         }
+    }
     }
 }
 
